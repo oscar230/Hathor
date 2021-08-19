f67a0243959e258cae53502ff93b10c9dca6a073
Oscar Andersson <oscar.andersson@kazoku.se>
2021-08-19 20:16:01 +0200

    Init dot net core project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hathor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
