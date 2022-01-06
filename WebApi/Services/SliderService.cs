c29d1acf9fae0cf0e37349ccb7129187e9e6921d
Oscar Andersson <oscar.andersson@kazoku.se>
2022-01-06 15:47:52 +0100

    Remove git cache

--- WebApi/Services/SliderService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebApi.Services
-{
-    public class SliderService
-    {
-    }
-}
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker-compose up --force-recreate --build -d
-docker image prune -f
-docker ps | grep "hathor"
