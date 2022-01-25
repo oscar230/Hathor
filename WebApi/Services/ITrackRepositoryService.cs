28cc11e580e21a5dda06cb91a355e9234b421385
Oscar Andersson <oscar.andersson@kazoku.se>
2022-01-25 21:53:37 +0100

    Adds track repositories

---
+++ WebApi/Services/ITrackRepositoryService.cs
@@ -0,0 +1,9 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public interface ITrackRepositoryService
+    {
+        Task<List<ITrack>> Query(string? query);
+    }
+}
---
+++ WebApi/Services/SliderTrackRepositoryService.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public class SliderTrackRepositoryService : ITrackRepositoryService
+    {
+        private const string SLIDER_API_QUERY = "https://slider.wonky.se/";
+        private readonly ILogger _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SliderTrackRepositoryService(ILogger logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public Task<List<ITrack>> Query(string? query)
+        {
+            query = query ?? string.Empty;
+            query = HttpUtility.UrlEncode(query);
+            var uri = new Uri($"{SLIDER_API_QUERY}{query}");
+            using (var client = _httpClientFactory.CreateClient())
+            {
+
+            }
+        }
+
+
+    }
+}
