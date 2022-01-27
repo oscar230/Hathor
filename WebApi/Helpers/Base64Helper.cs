e742b8d40e9d6c64acf6d487f0746b71cca9db1d
Oscar Andersson <oscar.andersson@kazoku.se>
2022-01-27 18:29:36 +0100

    Adds base64 helper

---
+++ WebApi/Helpers/Base64Helper.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Helpers
+{
+    public static class Base64Helper
+    {
+        public static string Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        public static string Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
