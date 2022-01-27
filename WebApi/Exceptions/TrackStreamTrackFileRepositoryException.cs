d38294564798b7d78c707a999eabc328979343ce
Oscar Andersson <oscar.andersson@kazoku.se>
2022-01-27 22:58:29 +0100

    Adds download failed excecption

---
+++ WebApi/Exceptions/TrackStreamTrackFileRepositoryException.cs
@@ -0,0 +1,16 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Exceptions
+{
+    public class TrackStreamTrackFileRepositoryException : Exception, IUserExceptions
+    {
+        private Uri _uri;
+
+        public TrackStreamTrackFileRepositoryException(Uri uri) : base($"Could not get stream from uri {uri}.")
+        {
+            _uri = uri;
+        }
+
+        public string UserMessage => $"Could not download file from uri {_uri}.";
+    }
+}
