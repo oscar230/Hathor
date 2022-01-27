0434c705f72d2b2eb92b7a2cb8139716f77e75b7
Oscar Andersson <oscar.andersson@kazoku.se>
2022-01-27 19:17:33 +0100

    Adds enumerable helper for randomizing lists

---
+++ WebApi/Helpers/EnumerableHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+namespace WebApi.Helpers
+{
+    public static class EnumerableHelper
+    {
+        private static readonly Random random = new();
+
+        public static IEnumerable<T> Shuffle<T>(IEnumerable<T> enumerable)
+        {
+            var list = enumerable.ToList();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            return list.AsEnumerable();
+        }
+
+        public static T RandomOne<T>(IEnumerable<T> enumerable)
+        {
+            var list = enumerable.ToList();
+            var randomNumber = random.Next(list.Count);
+            return list[randomNumber];
+        }
+    }
+}
