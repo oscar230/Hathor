8c0aaa5064eb0f0a859b1e0e98b5482ae59ff5cd
Oscar Andersson <oscar.andersson@kazoku.se>
2022-01-27 19:50:09 +0100

    Adds user agent service

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddLogging();
+builder.Services.AddScoped<IUserAgentService, UserAgentService>();
 builder.Services.AddScoped<ISliderTrackRepositoryService, SliderTrackRepositoryService>();
 builder.Services.AddHttpClient<ISliderTrackRepositoryService, SliderTrackRepositoryService>("SliderHttpClient", configureClient =>
 {
