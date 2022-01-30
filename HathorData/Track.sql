992150615e488f440d7a6e7687f1236ca94e58ab
Oscar Andersson <oscar.andersson@kazoku.se>
2022-01-30 18:03:40 +0100

    Adds track db

---
+++ HathorData/Track.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Track]
+(
+    [InternalID] VARCHAR(50) NOT NULL, 
+    [Guid] UNIQUEIDENTIFIER NOT NULL, 
+    [RepositoryGuid] UNIQUEIDENTIFIER NOT NULL, 
+    PRIMARY KEY ([Guid]) 
+)
