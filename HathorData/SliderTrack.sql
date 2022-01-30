fde7c88263f4bac9791a26814f2f1b160b32bcc9
Oscar Andersson <oscar.andersson@kazoku.se>
2022-01-30 18:18:23 +0100

    Adds slider track db

---
+++ HathorData/SliderTrack.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SliderTrack]
+(
+	[SliderID] VARCHAR(20) NOT NULL PRIMARY KEY, 
+    [Duration] TINYINT NOT NULL, 
+    [FullTitle] VARCHAR(200) NOT NULL, 
+    [Url] VARCHAR(250) NOT NULL, 
+    [ExtraInformation] BIT NULL DEFAULT 0
+)
