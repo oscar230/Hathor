322a27125ebd39e3f7caefa8ae6ec7f9e8418bef
Oscar Andersson <oscar.andersson@kazoku.se>
2022-03-29 21:15:17 +0200

    Uses flurl

---
+++ WebApi/Extensions/FlurlExtension.cs
@@ -0,0 +1,25 @@
+﻿using Flurl.Http;
+using WebApi.Services;
+
+namespace WebApi.Extensions
+{
+    public static class FlurlExtension
+    {
+        private static T WithRandomUserAgent<T>(this T clientOrRequest, UserAgentService userAgentService) where T : IHttpSettingsContainer
+        {
+            return clientOrRequest.WithHeader<T>("User-Agent", userAgentService.GetRandom());
+        }
+
+        public static T AtSlider<T>(this T clientOrRequest, UserAgentService userAgentService) where T : IHttpSettingsContainer
+        {
+            TimeSpan timeout = new(0, 3, 0);
+            return WithRandomUserAgent(clientOrRequest, userAgentService).WithTimeout(timeout).WithHeader("Accept", "audio/mpeg").WithHeader("Accept-Encoding", "gzip, deflate, br").WithHeader("Accept-Language", "en-US").WithHeader("Connection", "keep-alive").WithHeader("Host", "slider.kz").WithHeader("Upgrade-Insecure-Requests", "1");
+        }
+
+        public static T AtLiveTracklist<T>(this T clientOrRequest, UserAgentService userAgentService) where T : IHttpSettingsContainer
+        {
+            TimeSpan timeout = new(0, 0, 10);
+            return WithRandomUserAgent(clientOrRequest, userAgentService).WithTimeout(timeout);
+        }
+    }
+}
--- WebApi/Services/TrackRepositoryServices/SliderTrackRepositoryService.cs
+++ WebApi/Services/TrackRepositoryServices/SliderTrackRepositoryService.cs
@@ -1,8 +1,9 @@
-﻿using HathorCommon.Models;
-using System.Diagnostics;
-using System.Text.Json;
+﻿using Flurl.Http;
+using Flurl.Http.Configuration;
+using HathorCommon.Models;
 using System.Web;
 using WebApi.Exceptions;
+using WebApi.Extensions;
 using WebApi.Helpers;
 using WebApi.Models;
 using WebApi.Models.Slider;
@@ -11,93 +12,39 @@ namespace WebApi.Services.TrackRepositoryServices
 {
     public class SliderTrackRepositoryService : ITrackRepositoryService
     {
-        public const string DIRECT_SLIDER_HTTP_CLIENT_NAME = "DirectSliderKzHttpClient";
-        public const string PROXIED_SLIDER_HTTP_CLIENT_NAME = "ProxiedSliderKzHttpClient";
-
+        private const string BASE_URL = "https://slider.kz/";
         private readonly ILogger<SliderTrackRepositoryService> _logger;
-        private readonly HttpClient _httpClientDirectSlider;
-        private readonly HttpClient _httpClientProxiedSlider;
+        private readonly IFlurlClient _flurlClient;
         private readonly UserAgentService _userAgentService;
-        private readonly DbService _dbService;
 
-        public SliderTrackRepositoryService(ILogger<SliderTrackRepositoryService> logger, IHttpClientFactory httpClientFactory, UserAgentService userAgentService, DbService dbService)
+        public SliderTrackRepositoryService(ILogger<SliderTrackRepositoryService> logger, IFlurlClientFactory flurlClientFactory, UserAgentService userAgentService)
         {
             _logger = logger;
-            _httpClientDirectSlider = httpClientFactory.CreateClient(DIRECT_SLIDER_HTTP_CLIENT_NAME);
-            _httpClientProxiedSlider = httpClientFactory.CreateClient(PROXIED_SLIDER_HTTP_CLIENT_NAME);
+            _flurlClient = flurlClientFactory.Get(BASE_URL);
             _userAgentService = userAgentService;
-            _dbService = dbService;
         }
 
         public Repository Repository => RepositoryHelper.GetSliderRepository;
 
         public async Task<Stream> StreamTrackFile(Uri uri, CancellationToken cancellationToken)
         {
-            var stopWatch = new Stopwatch();
-            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
-            httpRequestMessage.Headers.Add("Accept", "audio/mpeg");
-            httpRequestMessage.Headers.Add("Accept-Encoding", "gzip, deflate, br");
-            httpRequestMessage.Headers.Add("Accept-Language", "en-US");
-            httpRequestMessage.Headers.Add("Connection", "keep-alive");
-            httpRequestMessage.Headers.Add("Host", "slider.kz");
-            httpRequestMessage.Headers.Add("Upgrade-Insecure-Requests", "1");
-            httpRequestMessage.Headers.Add("User-Agent", _userAgentService.RandomOne());
-            stopWatch.Start();
-            using (var httpResponseMessage = await _httpClientDirectSlider.SendAsync(httpRequestMessage))
+            Stream stream = await _flurlClient.Request(uri).AtSlider(_userAgentService).GetStreamAsync(cancellationToken);
+            if (stream is not null && !stream.Length.Equals(0))
             {
-                if (httpResponseMessage.IsSuccessStatusCode)
-                {
-                    var stream = await httpResponseMessage.Content.ReadAsStreamAsync(cancellationToken);
-                    stopWatch.Stop();
-                    var ts = stopWatch.Elapsed;
-                    _logger.LogDebug($"Stream track file took {ts.TotalSeconds} seconds (total {ts.Milliseconds} ms).");
-                }
-                else
-                {
-                    stopWatch.Stop();
-                    var ts = stopWatch.Elapsed;
-                    _logger.LogWarning($"Stream track file failed! uri: {httpRequestMessage.RequestUri}, status: {httpResponseMessage.StatusCode} ({((int)httpResponseMessage.StatusCode)}), took {ts.TotalSeconds} seconds.");
-                }
+                return stream;
             }
             throw new TrackStreamTrackFileRepositoryException(uri);
         }
 
         public async Task<IEnumerable<Track>> Query(string? query)
         {
-            var stopWatch = new Stopwatch();
-            HttpRequestMessage httpRequestMessage;
-            if (query == null)
+            string pathAndQuery = $"/#{HttpUtility.UrlEncode(query)}";
+            SliderTrackQueryResult trackQueryResult = await _flurlClient.Request(pathAndQuery).AtSlider(_userAgentService).GetJsonAsync<SliderTrackQueryResult>();
+            if (trackQueryResult is not null && trackQueryResult.SliderTrackList is not null && trackQueryResult.SliderTrackList.SliderTracks is not null && trackQueryResult.SliderTrackList.SliderTracks.Any()) 
             {
-                httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, string.Empty);
-            }
-            else
-            {
-                query = HttpUtility.UrlEncode(query);
-                httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, query);
-            }
-            stopWatch.Start();
-            using (var httpResponseMessage = await _httpClientProxiedSlider.SendAsync(httpRequestMessage))
-            {
-                if (httpResponseMessage.IsSuccessStatusCode)
-                {
-                    var content = await httpResponseMessage.Content.ReadAsStringAsync();
-                    var sliderTrackQueryResult = JsonSerializer.Deserialize<SliderTrackQueryResult>(content);
-                    if (sliderTrackQueryResult?.SliderTrackList?.SliderTracks?.Count() > 0 && sliderTrackQueryResult?.SliderTrackList?.SliderTracks?.FirstOrDefault()?.Id is not null)
-                    {
-                        stopWatch.Stop();
-                        var ts = stopWatch.Elapsed;
-                        _logger.LogDebug($"Query took {ts.TotalSeconds} seconds (total {ts.Milliseconds} ms).");
-                        var sliderTracks = sliderTrackQueryResult.SliderTrackList.SliderTracks;
-                        _dbService.AddSliderTracks(sliderTracks);
-                        return sliderTracks.Select(sliderTrack => new Track(sliderTrack));
-                    }
-                }
-                else
-                {
-                    stopWatch.Stop();
-                    var ts = stopWatch.Elapsed;
-                    _logger.LogWarning($"Query failed! uri: {httpRequestMessage.RequestUri}, status: {httpResponseMessage.StatusCode} ({((int)httpResponseMessage.StatusCode)}), took {ts.TotalSeconds} seconds.");
-                }
+                IEnumerable<Track> tracks = trackQueryResult.SliderTrackList.SliderTracks.Select(sliderTrack => new Track(sliderTrack));
+                _logger.LogDebug($"Slider query found {tracks.Count()} tracks for query {query}.");
+                return tracks;
             }
             throw new TrackQueryNotFoundInThisRepositoryException(query, Repository);
         }
