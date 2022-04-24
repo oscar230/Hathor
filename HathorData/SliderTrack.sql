4bb1c34bff391ce8d9acc7c7d8f4f45b2210e8ec
Oscar Andersson <2kafkimc@duck.com>
2022-04-24 13:04:16 +0200

    Removes data project

--- HathorData/SliderTrack.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[SliderTrack]
-(
-	[SliderID] VARCHAR(20) NOT NULL PRIMARY KEY, 
-    [Duration] INT NOT NULL, 
-    [FullTitle] VARCHAR(200) NOT NULL, 
-    [Url] VARCHAR(250) NOT NULL, 
-    [ExtraInformation] BIT NULL DEFAULT 0
-)
--- HathorData/Track.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Track]
-(
-    [InternalID] VARCHAR(50) NOT NULL, 
-    [Guid] UNIQUEIDENTIFIER NOT NULL, 
-    [RepositoryGuid] UNIQUEIDENTIFIER NOT NULL, 
-    PRIMARY KEY ([Guid]) 
-)
