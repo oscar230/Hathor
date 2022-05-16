fb5babd545473926b8b54521a4f6dedb2be1eaba
Oscar Andersson <11545617+oscar230@users.noreply.github.com>
2022-05-16 18:46:32 +0200

    Adds simple test case

---
+++ Hathor.Slider.Test/QueryTest.cs
@@ -0,0 +1,30 @@
+﻿using Hathor.Slider.Lib;
+using Hathor.Slider.Lib.Models.Slider;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hathor.Slider.Test
+{
+    [TestClass]
+    public class QueryTest
+    {
+        private readonly Mock<Logger<Query>> _mockLogger;
+
+        public QueryTest()
+        {
+            _mockLogger = new Mock<Logger<Query>>();
+        }
+
+        [TestMethod]
+        public async void Search()
+        {
+            const string QUERY = "Avicii Levels";
+            List<Track> tracks = await Query.Search(_mockLogger.Object, QUERY);
+            Assert.IsNotNull(tracks);
+            Assert.IsTrue(tracks.Any());
+        }
+    }
+}
