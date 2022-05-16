b837c984c65f25b3821a1afeece1e159729bf8be
Oscar Andersson <11545617+oscar230@users.noreply.github.com>
2022-05-16 22:11:26 +0200

    Adds test structure to slider tests

--- Hathor.Slider.Test/QueryTest.cs
+++ Hathor.Slider.Test/QueryTest.cs
@@ -1,4 +1,6 @@
-﻿using Hathor.Slider.Lib;
+﻿using Flurl.Http;
+using Flurl.Http.Testing;
+using Hathor.Slider.Lib;
 using Hathor.Slider.Lib.Models.Slider;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -11,18 +13,43 @@ namespace Hathor.Slider.Test
     [TestClass]
     public class QueryTest
     {
-        private readonly Mock<Logger<Query>> _mockLogger;
+        private Logger<Query>? _logger;
+        private HttpTest? _httpTest;
+        private IFlurlClient? _flurlClient;
+        private Query? _query;
 
-        public QueryTest()
+        [TestInitialize]
+        public void Setup()
         {
-            _mockLogger = new Mock<Logger<Query>>();
+            _logger = Mock.Of<Logger<Query>>();
+            _httpTest = new HttpTest();
+            _flurlClient = new FlurlClient();
+            _query = new Query(_logger, _flurlClient);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            if (_query is not null)
+            {
+                _query.Dispose();
+            }
+            if (_flurlClient is not null)
+            {
+                _flurlClient.Dispose();
+            }
+            if (_httpTest is not null)
+            {
+                _httpTest.Dispose();
+            }
         }
 
         [TestMethod]
-        public async void Search()
+        [DataRow(null)]
+        [DataRow("Levels")]
+        public async void Search(string? query)
         {
-            const string QUERY = "Avicii Levels";
-            List<Track> tracks = await Query.Search(_mockLogger.Object, QUERY);
+            List<Track> tracks = await Query.Search(query);
             Assert.IsNotNull(tracks);
             Assert.IsTrue(tracks.Any());
         }
