45ef2251c70ab06b8329e144eef8c10e74bf988d
Oscar Andersson <11545617+oscar230@users.noreply.github.com>
2022-06-12 21:20:20 +0200

    Move types to specific folder and fix namespace

---
+++ HathorCommon/Models/Abstracts/Types/MusicAlbumReleaseType.cs
@@ -0,0 +1,14 @@
+﻿namespace Hathor.Common.Models.Abstracts.Types
+{
+    /// <summary>
+    /// https://schema.org/MusicAlbumReleaseType
+    /// The kind of release which this album is: single, EP or album.
+    /// </summary>
+    internal enum MusicAlbumReleaseType
+    {
+        AlbumRelease,
+        BroadcastRelease,
+        EPRelease,
+        SingleRelease
+    }
+}
---
+++ HathorCommon/Models/Abstracts/Types/MusicReleaseFormatType.cs
@@ -0,0 +1,17 @@
+﻿namespace Hathor.Common.Models.Abstracts.Types
+{
+    /// <summary>
+    /// https://schema.org/MusicReleaseFormatType
+    /// Format of this release (the type of recording media used, ie. compact disc, digital media, LP, etc.).
+    /// </summary>
+    internal enum MusicReleaseFormatType
+    {
+        CDFormat,
+        CassetteFormat,
+        DVDFormat,
+        DigitalAudioTapeFormat,
+        DigitalFormat,
+        LaserDiscFormat,
+        VinylFormat
+    }
+}
