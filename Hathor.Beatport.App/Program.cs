d78fbb7c3e09dd6c0621de3a7882502235dfbd9d
Oscar Andersson <11545617+oscar230@users.noreply.github.com>
2022-06-12 17:56:48 +0200

    Adds beatport app project

---
+++ Hathor.Beatport.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using Flurl.Http;
+using Hathor.Common.Helpers;
+
+const string NAME = "Hathor Slider";
+FlurlClient _flurlClient = new("https://slider.kz");
+
+async Task<bool> MainMenu()
+{
+    return true;
+}
+
+ConsoleHelper.WriteLine($"Welcome to {NAME}.");
+while (await MainMenu()) { }
+ConsoleHelper.WriteLine("Bye! :)");
