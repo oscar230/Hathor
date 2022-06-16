ff3f917d764cbddb3de35a1493121d652ed01625
Oscar Andersson <11545617+oscar230@users.noreply.github.com>
2022-06-16 15:22:22 +0200

    Adds helper methods for handling URIs on beaports models

---
+++ Hathor.Beatport.Lib/Helpers/BeatportModelHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+
+namespace Hathor.Beatport.Lib.Helpers
+{
+    internal static class BeatportModelHelper
+    {
+        internal static string? SearchResultFromUrl(Uri? Url)
+        {
+            if (Url is not null)
+            {
+                string fullQuery = Url.Query;
+                var quries = HttpUtility.ParseQueryString(fullQuery);
+                string? searchQuery = quries["search"];
+                if (searchQuery is not null)
+                {
+                    return HttpUtility.UrlDecode(searchQuery);
+                }
+            }
+            return null;
+        }
+        internal static Uri BuildSearchQuery(Uri baseUrl, string searchQuery)
+        {
+            searchQuery = HttpUtility.UrlEncode(searchQuery) ?? string.Empty;
+            string relativeUri = new($"/search?q={searchQuery}");
+            return new(baseUrl, relativeUri);
+        }
+    }
+}
