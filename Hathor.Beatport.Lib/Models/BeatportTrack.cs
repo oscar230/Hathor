164dfcb323c4c1f5d6b5075cb115ac7222119480
Oscar Andersson <11545617+oscar230@users.noreply.github.com>
2022-06-26 20:15:43 +0200

    Decodes html in beaport models

--- Hathor.Beatport.Lib/Models/BeatportTrack.cs
+++ Hathor.Beatport.Lib/Models/BeatportTrack.cs
@@ -1,6 +1,7 @@
 ﻿using Hathor.Beatport.Lib.Helpers;
 using Hathor.Common.Models;
 using HtmlAgilityPack;
+using System.Web;
 
 namespace Hathor.Beatport.Lib.Models
 {
@@ -29,6 +30,9 @@ namespace Hathor.Beatport.Lib.Models
 
         public BeatportTrack(HtmlNode node, Uri uri)
         {
+            string title = node.SelectSingleNode(".//span[@class='buk-track-primary-title']").InnerText;
+            string version = node.SelectSingleNode(".//span[@class='buk-track-remixed']").InnerText;
+            string price = node.SelectSingleNode(".//div[@class='buy-button track-buy-button \n']").Attributes["data-price"].Value;
             string hrefPathString = node.SelectSingleNode(".//p[@class='buk-track-title']").SelectSingleNode("//a").Attributes["href"].Value;
             string artworkSrcString = node.SelectSingleNode(".//img[@class='buk-track-artwork lazy-load']").Attributes["src"].Value;
             string releasedDateString = node.SelectSingleNode(".//p[@class='buk-track-released']").InnerText;
@@ -36,20 +40,22 @@ namespace Hathor.Beatport.Lib.Models
             HtmlNodeCollection? remixersHtmlNodes = node.SelectNodes(".//p[@class='buk-track-remixers']/a");
             IEnumerable<BeatportArtist>? artists = artistsHtmlNodes is not null ? artistsHtmlNodes.Select(a => new BeatportArtist(a, uri)) : new List<BeatportArtist>();
             IEnumerable<BeatportArtist>? remixers = remixersHtmlNodes is not null ? remixersHtmlNodes.Select(a => new BeatportArtist(a, uri)) : new List<BeatportArtist>();
+            HtmlNode releaseHtmlNode = node.SelectSingleNode(".//div[@class='buk-track-artwork-parent']").SelectSingleNode("//a");
+            string key = node.SelectSingleNode(".//p[@class='buk-track-key']").InnerText;
 
             Id = int.Parse(node.Attributes["data-ec-id"].Value);
             Url = new Uri(uri, hrefPathString);
-            Title = node.SelectSingleNode(".//span[@class='buk-track-primary-title']").InnerText;
-            Version = node.SelectSingleNode(".//span[@class='buk-track-remixed']").InnerText;
-            Price = node.SelectSingleNode(".//div[@class='buy-button track-buy-button \n']").Attributes["data-price"].Value;
+            Title = HttpUtility.HtmlDecode(title);
+            Version = HttpUtility.HtmlDecode(version);
+            Price = HttpUtility.HtmlDecode(price);
             Artists = artists;
             Remixers = remixers;
             Artwork = new BeatportArtwork(new Uri(artworkSrcString));
             Length = default;
-            ReleasedDate = BeatportModelHelper.StringToDateTime(releasedDateString);
-            Release = new BeatportRelease(node.SelectSingleNode(".//div[@class='buk-track-artwork-parent']").SelectSingleNode("//a"), uri);
+            ReleasedDate = BeatportModelHelper.StringToDateTime(HttpUtility.HtmlDecode(releasedDateString));
+            Release = new BeatportRelease(releaseHtmlNode, uri);
             Bpm = default;
-            Key = node.SelectSingleNode(".//p[@class='buk-track-key']").InnerText;
+            Key = HttpUtility.HtmlDecode(key);
             Genre = new BeatportGenre(node.SelectSingleNode(".//p[@class='buk-track-genre']").SelectSingleNode("//a"), uri);
             Label = new BeatportLabel(node.SelectSingleNode(".//p[@class='buk-track-labels']").SelectSingleNode("//a"), uri);
         }
@@ -67,9 +73,9 @@ namespace Hathor.Beatport.Lib.Models
                 FileSizeInBytes = default,
                 SampleRateInHz = default,
                 BitRateInBitsPerSecond = default,
-                IsLyricsClean = default,
+                IsLyricsClean = false,
                 InAlbum = Release.ToAlbum(),
-                Comments = $"Label {Label}. Price {Price}. Version {Version}.",
+                Comments = $"Key {Key}. Version {Version}. Label {Label}. Price {Price}.",
                 Genres = Genre is not null ? new List<Genre>() { Genre.ToGenre() } : null,
                 Bpm = Bpm is not null ? float.Parse(Bpm) : default,
                 Key = Key,
