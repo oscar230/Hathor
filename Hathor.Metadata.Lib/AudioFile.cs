738f1fb42c9791a6d2a2691fe24a127a64c64b2b
Oscar Andersson <2kafkimc@duck.com>
2022-07-05 22:51:02 +0200

    Tag lib read and write using its mapper class

--- Hathor.Metadata.Lib/AudioFile.cs
+++ Hathor.Metadata.Lib/AudioFile.cs
@@ -5,12 +5,55 @@ namespace Hathor.Metadata.Lib
 {
     public class AudioFile
     {
-        public Track ReadTrack(FileInfo fileInfo) => ReadTrack(fileInfo.FullName);
+        private readonly string[] supportedFileExtension = new string[] { "mp3" };
+        public Track Read(string filePath) => Read(new FileInfo(filePath));
         
-        public Track ReadTrack(string filePath)
+        public Track Read(FileInfo fileInfo)
         {
-            TagLib.File audioFile = TagLib.File.Create(filePath);
-            return TagLibCommonMapper.MapTagLibFileToTrack(audioFile);
+            CheckIfFileExists(fileInfo);
+            CheckIfFileTypeIsSupported(fileInfo);
+            using (TagLib.File audioFile = TagLib.File.Create(fileInfo.FullName))
+            {
+                return TagLibMapper.ToTrack(audioFile);
+            }
+        }
+
+        public void Write(string filePath, Track track) => Write(new FileInfo(filePath), track);
+
+        public void Write(FileInfo fileInfo, Track track)
+        {
+            CheckIfFileExists(fileInfo);
+            CheckIfFileTypeIsSupported(fileInfo);
+            CheckIfFileIsReadOnly(fileInfo);
+            using (TagLib.File audioFile = TagLib.File.Create(fileInfo.FullName))
+            {
+                TagLibMapper.TrackToFile(audioFile, track);
+                audioFile.Save();
+            }
+        }
+
+        private void CheckIfFileTypeIsSupported(FileInfo fileInfo)
+        {
+            if (supportedFileExtension.Contains(fileInfo.Extension))
+            {
+                throw new InvalidOperationException($"Cannot read file {fileInfo.FullName}, the file type with extension {fileInfo.Extension} is not supported. The only supported file type extensions are: {string.Join(", ", supportedFileExtension)}.");
+            }
+        }
+
+        private void CheckIfFileIsReadOnly(FileInfo fileInfo)
+        {
+            if (fileInfo.IsReadOnly)
+            {
+                throw new FileLoadException("Cannot write to file, the is read only.", fileInfo.FullName);
+            }
+        }
+
+        private void CheckIfFileExists(FileInfo fileInfo)
+        {
+            if (!fileInfo.Exists)
+            {
+                throw new FileNotFoundException($"Cannot read metadata from a file that does not exist.", fileInfo.FullName);
+            }
         }
     }
 }
---
+++ Hathor.Metadata.Lib/Helpers/Mappers/TagLibMapper.cs
@@ -0,0 +1,86 @@
+﻿using Hathor.Common.Helpers;
+using Hathor.Common.Models;
+
+namespace Hathor.Metadata.Lib.Helpers.Mappers
+{
+    internal static class TagLibMapper
+    {
+        internal static Track ToTrack(TagLib.File file, Track? overwriteTrack = null)
+        {
+            const char artistSeparator = ',';
+            IEnumerable<Artist> artists = file.Tag.Performers.Any() ? file.Tag.Performers.Select(a => new Artist(a)) : (file.Tag.AlbumArtists.Select(a => new Artist(a)) ?? Array.Empty<Artist>());
+            IEnumerable<Artist> remixers = file.Tag.RemixedBy.Contains(artistSeparator).Equals(0) ? new Artist[] { new Artist(file.Tag.RemixedBy) } : file.Tag.RemixedBy.Split(artistSeparator).Where(r => r.Length > 0).Select(r => new Artist(r));
+            IEnumerable<Genre> genres = file.Tag.Genres.Select(g => new Genre(g));
+            string? version = TrackHelper.GetVersion(file.Name);
+            if (overwriteTrack is null)
+            {
+                return new Track()
+                {
+                    Uri = default,
+                    Title = file.Tag.Title,
+                    Artists = artists,
+                    Remixers = remixers,
+                    Year = (short)file.Tag.Year,
+                    Duration = file.Properties.Duration,
+                    FileSizeInBytes = file.Length,
+                    SampleRateInHz = file.Properties.AudioSampleRate,
+                    BitRateInBitsPerSecond = file.Properties.AudioBitrate,
+                    LyricVulgarity = TrackHelper.GetLyricVulgarity(file.Name),
+                    InAlbum = file.Tag.Album.Any() ? new Album(file.Tag.Album) : default,
+                    Comments = file.Properties.Description,
+                    Genres = genres,
+                    Bpm = file.Tag.BeatsPerMinute,
+                    Key = file.Tag.InitialKey,
+                    Version = version ?? string.Empty,
+                };
+            }
+            else
+            {
+                if (artists.Any())
+                {
+                    overwriteTrack.Artists = artists;
+                }
+                if (remixers.Any())
+                {
+                    overwriteTrack.Remixers = remixers;
+                }
+                overwriteTrack.Year = (short)file.Tag.Year;
+                overwriteTrack.Duration = file.Properties.Duration;
+                overwriteTrack.FileSizeInBytes = file.Length;
+                overwriteTrack.SampleRateInHz = file.Properties.AudioSampleRate;
+                overwriteTrack.BitRateInBitsPerSecond = file.Properties.AudioBitrate;
+                overwriteTrack.LyricVulgarity = TrackHelper.GetLyricVulgarity(file.Name);
+                if (file.Tag.Album.Any())
+                {
+                    overwriteTrack.InAlbum = new Album(file.Tag.Album);
+                }
+                overwriteTrack.Comments = file.Properties.Description;
+                overwriteTrack.Genres = genres;
+                overwriteTrack.Bpm = file.Tag.BeatsPerMinute;
+                overwriteTrack.Key = file.Tag.InitialKey;
+                if (version is not null)
+                {
+                    overwriteTrack.Version = version;
+                }
+                return overwriteTrack;
+            }
+        }
+
+        internal static TagLib.File TrackToFile(TagLib.File file, Track track)
+        {
+            IEnumerable<string>? artists = track?.Artists?.Select(a => a.ToString() ?? string.Empty).Where(s => !string.IsNullOrWhiteSpace(s)) ;
+            IEnumerable<string>? remixers = track?.Remixers?.Select(r => r.Name ?? string.Empty).Where(s => !string.IsNullOrWhiteSpace(s));
+            IEnumerable<string>? genres = track?.Genres?.Select(g => g.ToString() ?? string.Empty).Where(s => !string.IsNullOrWhiteSpace(s));
+            file.Tag.Title = track?.Title ?? string.Empty;
+            file.Tag.Performers = artists?.ToArray() ?? Array.Empty<string>();
+            file.Tag.AlbumArtists = artists?.ToArray() ?? Array.Empty<string>();
+            file.Tag.Genres = genres?.ToArray() ?? Array.Empty<string>();
+            file.Tag.BeatsPerMinute = track?.Bpm is not null ? (uint)track.Bpm : default;
+            file.Tag.Comment = track?.Comments ?? string.Empty;
+            file.Tag.DateTagged = DateTime.UtcNow;
+            file.Tag.Year = track?.Year is not null ? (uint)track.Year : default;
+            file.
+            return file;
+        }
+    }
+}
