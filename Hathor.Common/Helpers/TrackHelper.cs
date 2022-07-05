53c7b12c9c5ffd9bab30246495874b2ea50bcc07
Oscar Andersson <2kafkimc@duck.com>
2022-07-05 22:50:13 +0200

    Adds helper methods to parse tracks full name

---
+++ Hathor.Common/Helpers/TrackHelper.cs
@@ -0,0 +1,41 @@
+﻿using Hathor.Common.Models;
+
+namespace Hathor.Common.Helpers
+{
+    public static class TrackHelper
+    {
+        public static LyricVulgarity GetLyricVulgarity(string fullName)
+        {
+            if (fullName.Contains('(') || fullName.Contains('['))
+            {
+                if (fullName.ToLower().Contains("clean"))
+                {
+                    return LyricVulgarity.Clean;
+                }
+                else if (fullName.ToLower().Contains("dirty"))
+                {
+                    return LyricVulgarity.Dirty;
+                }
+            }
+            return LyricVulgarity.Unknown;
+        }
+
+        public static string? GetVersion(string fullName)
+        {
+            if (fullName.Contains('(') || fullName.Contains('['))
+            {
+                string fullNameLower = fullName.ToLower();
+                switch (fullNameLower)
+                {
+                    case "extended":
+                        return "Extended";
+                    case "original":
+                        return "Original";
+                    default:
+                        break;
+                }
+            }
+            return null;
+        }
+    }
+}
