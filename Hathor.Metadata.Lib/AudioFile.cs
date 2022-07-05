4a300b4265e0e44d8525005312f4dba933320e31
Oscar Andersson <11545617+oscar230@users.noreply.github.com>
2022-07-05 18:15:57 +0200

    Adds tag lib

---
+++ Hathor.Metadata.Lib/AudioFile.cs
@@ -0,0 +1,16 @@
+﻿using Hathor.Common.Models;
+using Hathor.Metadata.Lib.Helpers.Mappers;
+
+namespace Hathor.Metadata.Lib
+{
+    public class AudioFile
+    {
+        public Track ReadTrack(FileInfo fileInfo) => ReadTrack(fileInfo.FullName);
+        
+        public Track ReadTrack(string filePath)
+        {
+            TagLib.File audioFile = TagLib.File.Create(filePath);
+            return TagLibCommonMapper.MapTagLibFileToTrack(audioFile);
+        }
+    }
+}
---
+++ Hathor.Metadata.Lib/Helpers/Mappers/TagLibCommonMapper.cs
@@ -0,0 +1,45 @@
+﻿using Hathor.Common.Models;
+
+namespace Hathor.Metadata.Lib.Helpers.Mappers
+{
+    internal static class TagLibCommonMapper
+    {
+        internal static Track MapTagLibFileToTrack(TagLib.File file)
+        {
+            throw new NotImplementedException();
+            return new Track()
+            {
+                Uri = default,
+                Title = default,
+                Artists = default,
+                Remixers = default,
+                Year = default,
+                Duration = default,
+                FileSizeInBytes = default,
+                SampleRateInHz = default,
+                BitRateInBitsPerSecond = default,
+                LyricVulgarity = default,
+                InAlbum = default,
+                Comments = default,
+                Genres = default,
+                Bpm = default,
+                Key = default,
+                Version = default
+            };
+        }
+
+        internal static TagLib.File MapTrackToTagLibFile(TagLib.File file, Track track)
+        {
+            string?[] artists = track?.Artists?.Select(a => a.ToString()).ToArray() ?? Array.Empty<string>();
+            string?[] genres = track?.Genres?.Select(g => g.ToString()).ToArray() ?? Array.Empty<string>();
+            file.Tag.Title = track?.Title ?? string.Empty;
+            file.Tag.Performers = artists;
+            file.Tag.AlbumArtists = artists;
+            file.Tag.BeatsPerMinute = track?.Bpm is not null ? (uint)track.Bpm : default;
+            file.Tag.Comment = track?.Comments ?? string.Empty;
+            file.Tag.DateTagged = DateTime.UtcNow;
+            file.Tag.Genres = genres;
+            return file;
+        }
+    }
+}
