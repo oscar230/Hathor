33c0d751693121098749a3bd0060bfb5eba6d261
Oscar Andersson <2kafkimc@duck.com>
2022-07-13 20:57:33 +0200

    Adds core havit functionality

--- Hathor.Web/Program.cs
+++ Hathor.Web/Program.cs
@@ -1,10 +1,13 @@
 using Hathor.Web.Data;
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHxServices();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
@@ -14,7 +17,6 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
-
 
 app.UseStaticFiles();
 
