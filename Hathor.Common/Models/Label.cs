cd3207fe69d6b4a30751e60a6f324e98cef0b5e0
Oscar Andersson <2kafkimc@duck.com>
2022-11-06 13:41:12 +0100

    Remove old models

--- Hathor.Common/Models/Label.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hathor.Web.Models
-{
-    public class Label
-    {
-        public Uri? Uri { get; set; }
-        public string? Title { get; set; }
-        public Uri? Artwork { get; set; }
-    }
-}
--- Hathor.Common/Models/LyricVulgarity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hathor.Web.Models
-{
-    public enum LyricVulgarity : sbyte
-    {
-        Unknown = -1,
-        Clean = 0,
-        Dirty = sbyte.MaxValue // 127
-    }
-}
